30045a2640d55d714bcbc6a6cdbc8f79e513955a
ゆろち <yuro.7878@gmail.com>
2026-01-09 22:54:36 +0900

    Velopackの自動更新を統合

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Threading.Tasks;
+using System.Windows;
+using Velopack;
+using Velopack.Sources;
 
 namespace TurnOffTheDisplay;
 
@@ -7,4 +10,29 @@ namespace TurnOffTheDisplay;
 /// </summary>
 public partial class App : Application
 {
+    private const string UpdateRepositoryUrl = "https://github.com/1llum1n4t1s/TurnOffTheDisplay";
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        VelopackApp.Build().Run();
+
+        await CheckForUpdatesAsync();
+    }
+
+    private static async Task CheckForUpdatesAsync()
+    {
+        var source = new GithubSource(UpdateRepositoryUrl, "TurnOffTheDisplay", prerelease: false);
+        using var updateManager = new UpdateManager(source);
+
+        var updateInfo = await updateManager.CheckForUpdatesAsync();
+        if (updateInfo is null)
+        {
+            return;
+        }
+
+        await updateManager.DownloadUpdatesAsync(updateInfo);
+        updateManager.ApplyUpdatesAndRestart(updateInfo);
+    }
 }
